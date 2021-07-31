fe89d29e746cd9b3e980eababc250ad4dc0d2b4b
Mehmet COŞKUN <tekiner65@hotmail.com>
2021-07-31 12:44:48 +0300

    Update User.js

--- graphql/resolvers/queries/User.js
+++ graphql/resolvers/queries/User.js
@@ -1,7 +1,16 @@
 const User = {
     watchlist: async (parent, args, { WatchList }) => {
-        return await WatchList.findAll({ where: { user_id: parent.id }, order: [['id', 'DESC']] });
-    }
+        return await WatchList.findAll({
+            where: { user_id: parent.id },
+            order: [['id', 'DESC']],
+        });
+    },
+    keepwatchinglist: async (parent, args, { KeepWatchingList }) => {
+        return await KeepWatchingList.findAll({
+            where: { user_id: parent.id },
+            order: [['id', 'DESC']],
+        });
+    },
 };
 
 module.exports = User;
